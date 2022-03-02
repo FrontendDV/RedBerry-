b2d4adf395187254a275146652adf0f23760c943
FrontendDV <90509973+FrontendDV@users.noreply.github.com>
2022-03-02 16:17:58 +0400

    add cleave.js library
    add cleave js library to allow number input type on Georgian format

--- JS/main.js
+++ JS/main.js
@@ -24,40 +24,45 @@ next__btn.addEventListener('click',()=>{
 // Functions
 function check__inputs(){
     // Name input Check
-    if(name__input.value == '' || name__input.value.length <= 2){
+    if(name__input.value == ''){
         name__input.style.border = '1px solid var(--color-red)'
+        document.querySelector('.name__alert').innerText = '* first name is required'
+        document.querySelector('.name__alert').style.visibility = 'visible'
+    }else if(name__input.value.length < 2){
+        name__input.style.border = '1px solid var(--color-red)'
+        document.querySelector('.name__alert').innerText = '* Must be at least 2 letters '
         document.querySelector('.name__alert').style.visibility = 'visible'
     }else{
         name__input.style.border = '1px solid #525557'
         document.querySelector('.name__alert').style.visibility = 'hidden'
     }
     // Surname input Check
-    if(surname__input.value == '' || surname__input.value.length <= 2){
+    if(surname__input.value == ''){
         surname__input.style.border = '1px solid var(--color-red)'
+        document.querySelector('.surname__alert').innerText = '*last name is required'
+        document.querySelector('.surname__alert').style.visibility = 'visible'
+    }else if(surname__input.value.length < 2){
+        surname__input.style.border = '1px solid var(--color-red)'
+        document.querySelector('.surname__alert').innerText = '*Must be at least 2 letters'
         document.querySelector('.surname__alert').style.visibility = 'visible'
     }else{
         surname__input.style.border = '1px solid #525557'
         document.querySelector('.surname__alert').style.visibility = 'hidden'
     }
     // Email input Check
-    if(email__input.avlue == ''){
+    if(email__input.value == ''){
         email__input.style.border = '1px solid var(--color-red)'
+        document.querySelector('.email__alert').innerText = '*Email is required'
         document.querySelector('.email__alert').style.visibility = 'visible'
     }else if(!isEmail(email__input.value)){
         email__input.style.border = '1px solid var(--color-red)'
+        document.querySelector('.email__alert').innerText = '*Email must be the mail format '
         document.querySelector('.email__alert').style.visibility = 'visible'
     }else{
         email__input.style.border = '1px solid #525557'
         document.querySelector('.email__alert').style.visibility = 'hidden'
     }
-    // Number
-    if(!isPhoneNumber(number__input.value)){
-        number__input.style.border = '1px solid var(--color-red)'
-        document.querySelector('.number__alert').style.visibility = 'visible'
-    }else{
-        number__input.style.border = '1px solid #525557'
-        document.querySelector('.number__alert').style.visibility = 'hidden'
-    }
+    // Phone number check function is in index.html file
 }
 
 
@@ -68,10 +73,8 @@ function isEmail(email){
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 }
 
-// Check if is phone number
-function isPhoneNumber(number){
-    return /^[+995]\d{3}\d{2}\d{2}\d{2}\d{2}$/.test(number)
-}
+
+
 
 
 
